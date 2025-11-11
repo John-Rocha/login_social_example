e6159391166da3837946bfb629beb8a78d5aade8
Johnathan Rocha <johnathanrocha@gmail.com>
2025-11-11 19:48:11 -0300

    feat: Ensure Flutter bindings are initialized before Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:login_social_example/firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
